20cddae7b0eab50bb6b68f082563e0d9f469121d
Michael Navarro <navarro.michael93@gmail.com>
2020-03-03 20:40:30 -0600

    Import required libraries

--- sorter.py
+++ sorter.py
@@ -1,3 +1,7 @@
 #Read files in root directory
 #Create a folder for each file extension found if no folder exists already
 #Move files into their respective folders
+
+import os
+import sys
+import shutil
