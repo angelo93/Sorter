0bee364d1dca608304d43fca1b0df526154c7255
Michael Navarro <navarro.michael93@gmail.com>
2020-03-07 11:39:35 -0600

    Create dir to hold lists

--- write.py
+++ write.py
@@ -1,5 +1,19 @@
+import os
+
+save_path = os.getcwd() + '\\lists'
+
+def make_dir():
+  try:
+    os.mkdir(save_path)
+  except:
+    print('List folder already exists proceeding to write file')
+
 def txt_list(list_name, file_name):
-  with open(file_name, "w") as text_file:
+  complete_name = os.path.join(save_path, file_name)
+
+  make_dir()
+
+  with open(complete_name, "w") as text_file:
     for item in list_name:
       text_file.write(item + '\n')
 
